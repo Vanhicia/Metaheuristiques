c7223d1c1c3a6903505ffcf55ad79734533e76d4
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-04-16 14:55:36 +0200

    update tree structure

--- Fire_escape_project/src/tree_structure.py
+++ Fire_escape_project/src/tree_structure.py
@@ -1,19 +1,25 @@
 class Node:
     """ A Node is defined by
+    - its id
     - its father (a node)
     - a list of edges which lead to its sons)
     """
 
-    def __init__(self):
-        self._father=None
-        self._sons=[]
+    def __init__(self, id1):
+        self.id=id1
+        self.father=None
+        self.sons=[]
 
-    def __init__(self, father):
-        self._father=father
-        self._sons=[]
+    def __init__(self, id1, father):
+        self.id = id1
+        self.father=father
+        self.sons=[]
 
     def add_edge(self, edge):
-        self._sons.append(edge)
+        self.sons.append(edge)
+
+    def set_father(self, father):
+        self.father=father
 
 
 class EvacNode(Node):
@@ -21,9 +27,9 @@ class EvacNode(Node):
     - a population
     - a max rate
     """
-    def __init__(self, population):
-        Node.__init__(self)
-        self._population=population
+    def __init__(self, id1, population):
+        Node.__init__(self, id1)
+        self.population=population
 
 class Edge:
     """ An Edge is defined by
@@ -32,12 +38,25 @@ class Edge:
     - length
     - capacity
     """
-     def _init__(self, father, son, due_date, length, capacity):
-         self._father=father
-         self._son= son
-         self._due_date=due_date
-         self._length=length
-         self._capacity=capacity
+
+    def __init__(self, father, son):
+        self.father = father
+        self.son = son
+        self.due_date = -1
+        self.length = -1
+        self.capacity = -1
+
+     def __init__(self, father, son, due_date, length, capacity):
+         self.father=father
+         self.son= son
+         self.due_date=due_date
+         self.length=length
+         self.capacity=capacity
+
+    def add_info(self, due_date, length, capacity):
+         self.due_date=due_date
+         self.length=length
+         self.capacity=capacity
 
 
 class Tree:
@@ -46,4 +65,4 @@ class Tree:
         """
 
     def __init__(self, node):
-        self._root=node
+        self.root=node
