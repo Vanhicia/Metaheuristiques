6d65a6927f09498a4ef75f3b30f975ce36daa9eb
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-04-29 09:05:02 +0200

    implement the Data class and improve Node and Arc classes

---
+++ Fire_escape_project/src/data_structure.py
@@ -0,0 +1,232 @@
+class Data:
+    """ Attributes:
+        - a dictionary of nodes
+        - a dictionary of arcs
+        - the id of the safe node
+        - the list of evac node ids
+    """
+
+    def __init__(self, id1):
+        self.nodes = {}
+        self.arcs = {}
+        self.safe_node_id = id1
+        self.evac_node_id_list = []
+
+    def get_nodes(self):
+        return self.nodes
+
+    def get_arcs(self):
+        return self.arcs
+
+    def get_safe_node_id(self):
+        return self.safe_node_id
+
+    def get_evac_node_id_list(self):
+        return self.evac_node_id_list
+
+    # Add the node if it doesn't exist yet
+    def add_node(self, id1):
+        self.nodes.setdefault(id1, Node(id1))
+
+    # Add the node if it doesn't exist yet
+    def add_evac_node(self, id1, population, max_rate):
+        self.nodes.setdefault(id1, EvacNode(id1, population, max_rate))
+        self.evac_node_id_list.append(id1)
+
+    # Add the arc if it doesn't exist yet but the two nodes exist
+    def add_arc(self, father_id, son_id):
+        try:
+            # Check both nodes exist
+            _node1 = self.nodes[father_id]
+            _node2 = self.nodes[son_id]
+            # Create the arc if it doesn't exist
+            self.arcs.setdefault((father_id, son_id), Arc(self, father_id, son_id))
+        except KeyError:
+            print("add_arc: This arc can't be added")
+
+    # Add information about the arc between the nodes id1 and id2
+    # The order of the nodes doesn't matter
+    def add_arc_info(self, id1, id2, due_date, length, capacity):
+        # Look for the arc
+        try:
+            arc = self.arcs[(id1, id2)]
+            # Add information about the arc
+            arc.add_info(due_date, length, capacity)
+        except KeyError:
+            try:
+                arc = self.arcs[(id2, id1)]
+                # Add information about the arc
+                arc.add_info(due_date, length, capacity)
+            except KeyError:
+                # The arc has not been found, it is certainly a useless arc
+                # So we do nothing
+                print("add_arc_info: not info add because the arc doesn't exist")
+
+    # Return the node id1
+    # Return None if it doesn't exist
+    def find_node(self, id1):
+        try:
+            node = self.nodes[id1]
+        except KeyError:
+            node = None
+            print("find_node: This Node has not been found")
+        return node
+
+    # Return the arc between the nodes id1 and id2
+    # Return None if the arc doesn't exist
+    def find_arc(self, id1, id2):
+        try:
+            arc = self.arcs[(id1, id2)]
+        except KeyError:
+            try:
+                arc = self.arcs[(id2, id1)]
+            except KeyError:
+                arc = None
+                print("find_arc : this arc has not been found !")
+        return arc
+
+    def print_tree(self):
+        node = self.nodes[self.safe_node_id]
+        node.print_tree()
+
+
+class Node:
+    """ A Node is defined by
+    - its id
+    - the arc which leads to its father
+    - a list of arcs which lead to its sons)
+    """
+
+    def __init__(self, id1):
+        self.id = id1
+        self.father = None
+        self.sons = []
+
+    def get_id(self):
+        return self.id
+
+    def get_father(self):
+        return self.father
+
+    def get_sons(self):
+        return self.sons
+
+    def add_arc_successor(self, arc):
+        self.sons.append(arc)
+
+    def set_father(self, arc):
+        if self.father is None:
+            self.father = arc
+        else:
+            print("set_father: This node has already a father")
+
+    # Return None if the Node doesn't have this son
+    # Else return the arc between the current node and the indicated node
+    def find_arc_successor(self, node_id):
+        arc = None
+        k = 0
+        n = len(self.sons)
+        while arc.equals(None) and k < n:
+            node = self.sons[k]
+            if node.id == node_id:
+                arc = self.sons[k]
+            k += 1
+        return arc
+
+    # Print the tree in the console
+    def print_tree(self, k=0):
+        deb = ""
+
+        for i in range(k):
+            deb += "        "
+
+        deb += str(self.id) + " -> "
+
+        # Print the sons of the node
+        for son in self.sons:
+            print(deb + str(son.get_son().id))
+
+        # Recursion
+        k += 1
+        for son in self.sons:
+            son.get_son().print_tree(k)
+
+
+class EvacNode(Node):
+    """An EvacNode is a Node with
+    - a population
+    - a max rate
+    """
+
+    def __init__(self, id1, population, max_rate):
+        Node.__init__(self, id1)
+        self.population = population
+        self.max_rate = max_rate
+
+    def get_population(self):
+        return self.population
+
+    def get_max_rate(self):
+        return self.max_rate
+
+
+class Arc:
+    """ An Arc is defined by
+    - two Node (the father and the son)
+    - due date
+    - length
+    - capacity
+    """
+
+    def __init__(self, data, father_id, son_id, due_date=-1, length=-1, capacity=-1):
+        self.father = data.nodes[father_id]
+        self.son = data.nodes[son_id]
+        self.due_date = due_date
+        self.length = length
+        self.capacity = capacity
+
+        # Add the arc in the structure of both implied nodes
+        self.father.add_arc_successor(self)
+        self.son.set_father(self)
+
+    def add_info(self, due_date, length, capacity):
+        self.due_date = due_date
+        self.length = length
+        self.capacity = capacity
+
+    def get_father(self):
+        return self.father
+
+    def get_son(self):
+        return self.son
+
+    def get_due_date(self):
+        return self.due_date
+
+    def get_length(self):
+        return self.length
+
+    def get_capacity(self):
+        return self.capacity
+
+
+# Test
+"""
+if __name__ == '__main__':
+    data = Data(0)
+    data.add_node(0)
+    data.add_node(1)
+    data.add_node(2)
+    data.add_evac_node(3, 10, 5)
+    data.add_evac_node(4, 8, 2)
+    data.add_evac_node(5, 6, 3)
+
+    data.add_arc(0, 1)
+    data.add_arc(0, 2)
+    data.add_arc(1, 3)
+    data.add_arc(1, 4)
+    data.add_arc(2, 5)
+
+    data.print_tree()
+"""
+
--- Fire_escape_project/src/reader.py
+++ Fire_escape_project/src/reader.py
@@ -1,16 +1,16 @@
 from pathlib import Path
-from tree_structure import *
+from data_structure import *
 
 
 class Reader:
 
     def __init__(self, path):
         self.path = path  # Path containing the name of a file (string)
-        self.tree = get_tree_from_file(path)  # Generated Tree from the file
+        self.data = get_data_from_file(path)  # Generated Tree from the file
 
 
 #  Read a file and return the associated tree
-def get_tree_from_file(path):
+def get_data_from_file(path):
 
     # Open the file
     data_folder = Path("../instances/")
@@ -21,7 +21,7 @@ def get_tree_from_file(path):
     count = 1  # counter for changing line
     info = 0  # 0 : we are not in a section / 1: we are in evacuation info section / 2: we are in graph info section
     header = 0  # 0 : we are not on the header / 1: we are on the header of a section (Evacuation info, Graph info)
-    tree = None
+    data = None
 
     while line:
 
@@ -40,26 +40,26 @@ def get_tree_from_file(path):
                 if header == 1:  # We are on the header: <num evac nodes> <id of safe node>
                     [_, id_safe_node] = line.split(" ")
                     header = 0
-                    tree = Tree(Node(id_safe_node))
+                    data = Data(id_safe_node)
                 else:  # We are on a definition of an evac_node (line)
-                    treat_one_evac_node(line, tree)
+                    treat_one_evac_node(line, data)
 
             # Graph info
             elif info == 2:
                 if header == 1:  # We are on the header: <num nodes> <num edges>
                     header = 0
-                else:  # We are on a definition of an edge (line): <node 1> <node 2> <duedate> <length> <capacity>
-                    [node1, node2, due_date, length, capacity] = line.split(" ")
-                    update_one_edge_info(tree, node1, node2, due_date, length, capacity)
+                else:  # We are on a definition of an edge (line): <node 1> <node 2> <due date> <length> <capacity>
+                    [node1_id, node2_id, due_date, length, capacity] = line.split(" ")
+                    data.add_arc_info(node1_id, node2_id, due_date, length, capacity)
 
             else:
                 raise Exception("Error : We are not in a section such as Evacuation info or Graph info.")
         count += 1
-    return tree
+    return data
 
 
 #  We add the path from the evac_node to the safe node
-def treat_one_evac_node(line, tree):
+def treat_one_evac_node(line, data):
     evac_node_info = line.split(" ")  # We get all information of the evac node
 
     id_evac_node = evac_node_info[0]  # id of evac_node
@@ -67,27 +67,19 @@ def treat_one_evac_node(line, tree):
     max_rate = evac_node_info[2]  # max rate of evac node
     k = evac_node_info[3]  # number of parts in the evacuation route
 
-    evac_node = EvacNode(id_evac_node, population, max_rate)  # We create the evac node with its information
+    data.add_evac_node(id_evac_node, population, max_rate)  # We create the evac node with its information
 
-    son = evac_node
+    id_son = id_evac_node
 
     for i in range(0, k):
         id_father = evac_node_info[4 + i]
 
-        if not(tree.node_exist(id_father)):  # The parent node does not exist
-            father = Node(id_father)  # We create it
-        else:
-            father = tree.get_node_by_id(id_father) # The parent node exists, we get it
+        data.add_node(id_father) # The node is added only if it doesn't exist yet
 
-        father.add_edge(Edge(father, son))  # We update its sons list
-        son.set_father(father)  # We set the father for the son node
-        son = father
+        data.add_arc(id_father, id_son) # The arc is added only if it doesn't exist yet
 
+        id_son = id_father
 
-def update_one_edge_info(tree, node1, node2, due_date, length, capacity):
-    edge = tree.find_edge(node1, node2)
-    if edge is not None:  # The edge exists
-        edge.add_info(due_date, length, capacity)  # We update its info
 
 # Tests unitaires
 # data_folder = Path("../instances/")
