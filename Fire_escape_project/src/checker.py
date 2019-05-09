b022a3a119a7b79a03e357246cee9e97a0d9dc66
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-05-09 09:59:00 +0200

    check the capacity is not exceeded

--- Fire_escape_project/src/checker.py
+++ Fire_escape_project/src/checker.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 from data_structure import *
 from reader import *
+import numpy as np
 
 
 class Checker:
     """ Attributes:
     - instance : file name of the instance
-    - evac_nodes : list for the evac nodes (id, evac_rate, start_date)
+    - evac_nodes : dictionary for the evac nodes (key = id, info = evac_rate & start_date)
     - valid : true if the solution is valid
     - objective : value of the objective function
     """
@@ -21,12 +22,12 @@ class Checker:
 
             self.instance = f.readline().rstrip('\n\r')
             evac_node_nb = int(f.readline())
-            self.evac_nodes = []
+            self.evac_nodes = {}
 
             # Evac node information
             for k in range(evac_node_nb):
-                id, evac_rate, start_date = f.readline.split(" ")
-                self.evac_nodes.append({"id": id, "evac_rate": evac_rate, "start_date": start_date})
+                id1, evac_rate, start_date = f.readline.split(" ")
+                self.evac_nodes[id1] = {"evac_rate": evac_rate, "start_date": start_date}
 
             valid = f.readline().rstrip('\n\r')
             if valid == "valid":
@@ -39,8 +40,39 @@ class Checker:
             self.objective = int(f.readline())
 
     def check_solution(self):
+        time_limit = 1000
         reader = Reader(self.instance)
         data = reader.data
+        arc_nb = data.arcs.len()
 
+        guant = np.zeros(arc_nb, time_limit)
 
-        #TODO
+        k = 0
+        for arc in data.arcs:
+            for evac in arc.evac:
+                evac_node = data.nodes[evac.key()]
+                evac_info = self.evac_nodes[evac_node.id]
+
+                beg = evac_info['start_time'] + arc.time
+                if evac_node.population%evac_info['evac_rate'] == 0:
+                    end = beg + (evac_node.population//evac_info['evac_rate'])
+                else:
+                    end = beg + (evac_node.population // evac_info['evac_rate']) + 1
+
+                print("end = %d", end)
+                for i in range(beg, end):
+                    guant[k][i] += evac_info['evac_rate']
+
+            # Check the capacity is not exceeded
+            for i in range (time_limit):
+                # If the solution is not valid
+                if arc.capacity < guant[k][i]:
+                    print("The solution is invalid !")
+                    if not self.valid:
+                        return True
+                    else:
+                        return False
+
+            k += 1
+
+        print("The solution is valid")
