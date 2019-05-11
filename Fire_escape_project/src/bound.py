ab45f6686998b609de6b63d0e36534348c845687
KimiNako <katran@etud.insa-toulouse.fr>
2019-05-11 12:33:56 +0200

    Code cleaning

--- Fire_escape_project/src/bound.py
+++ Fire_escape_project/src/bound.py
@@ -1,4 +1,6 @@
 import math
+
+
 class Bound:
     def __init__(self, tree):
         self.tree = tree
@@ -42,7 +44,7 @@ class Bound:
                     if coef > 1:
                         size_package_at_start *= coef
 
-                    # If the capacity's section is too small
+                    # If the capacity's section is too small,
                     # we decrease the number of people to evacuate for one clock
                     if size_package_at_start > capacity:
                         size_package_at_start = math.ceil(population/capacity)
@@ -70,8 +72,6 @@ class Bound:
         for id_evac_node in self.tree.evac_node_id_list:
             lower_bound_per_evac_node.append(self.get_lower_bound_for_one_evac_node(id_evac_node))
         return max(lower_bound_per_evac_node)
-
-
 
     def calculate_upper_bound(self):
         pass
