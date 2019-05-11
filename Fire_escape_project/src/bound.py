a5e96a5fa4cccfe93eacbabaf17a7910d836f4c0
KimiNako <katran@etud.insa-toulouse.fr>
2019-05-11 12:05:53 +0200

    Add lower bound feature

--- Fire_escape_project/src/bound.py
+++ Fire_escape_project/src/bound.py
@@ -1,5 +1,3 @@
-from data_structure import *
-
 
 class Bound:
     def __init__(self, tree):
@@ -7,34 +5,68 @@ class Bound:
         self.lower_bound = None
         self.upper_bound = None
 
+    def get_lower_bound_for_one_evac_node(self, id_evac_node):
+        clock = 0
+
+        # We get information on where to start
+        id_start = id_evac_node
+        node_start = self.tree.find_node(id_evac_node)
+        section = node_start.father
+
+        population = node_start.population
+        arrived_population = 0
+        size_package_at_start = 0
+
+        start = 1
+        coef = 1
+
+        while id_start != self.tree.safe_node_id:
+
+            # We get information of the current section
+            time = section.time
+            capacity = section.capacity
+
+            period = 0
+
+            while period < time:
+                if population > 0:
+                    # We get the number of people we can evacuate for one clock
+                    if start:
+                        size_package_at_start += capacity
+                        start = 0
+
+                    coef = int(capacity/size_package_at_start)
+
+                    # If the capacity's section is enough to put more people,
+                    # we increase the number of people to evacuate
+                    if coef > 1:
+                        size_package_at_start *= coef
+
+                    population -= size_package_at_start
+                    arrived_population += size_package_at_start
+
+                # We count the time we are in this section
+                period += 1
+
+            clock += period
+
+            if population <= 0:  # We arrived at the end of a section
+                # We reinitialize population variable
+                population = arrived_population
+                arrived_population = 0
+                # We change section
+                id_start = section.get_father().get_id()
+                section = self.tree.find_node(id_start).get_father()
+
+        return clock + int(population/(coef*size_package_at_start))
+
     def calculate_lower_bound(self):
         lower_bound_per_evac_node = []
         for id_evac_node in self.tree.evac_node_id_list:
-            period = 0
-            id_start = id_evac_node
-            node_start = self.tree.find_node(id_evac_node)
-            population = node_start.get_population()
-            arrived_population = 0
-            arc_father = node_start.get_father()
-            cpt = 0
-            while id_start != self.tree.safe_node_id and cpt < 20:
-                cpt += 1
-                time = arc_father.get_time()
-                print("capacity")
-                capacity = arc_father.get_capacity()
-                while period < time:
-                    if population > 0:
-                        population -= capacity
-                        arrived_population += capacity
-                        print(population)
-                    period += 1
-                if population <= 0:
-                    population = arrived_population
-                    id_start = arc_father.get_father().get_id()
-                    arc_father = self.tree.find_node(id_start).get_father()
-
-            lower_bound_per_evac_node.append(period)
+            lower_bound_per_evac_node.append(self.get_lower_bound_for_one_evac_node(id_evac_node))
         return max(lower_bound_per_evac_node)
+
+
 
     def calculate_upper_bound(self):
         pass
