a2bff90788f5c2bfefc1826b821de80c56d5dc9b
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-05-24 09:56:47 +0200

    modificate upper bound

--- Fire_escape_project/src/bound.py
+++ Fire_escape_project/src/bound.py
@@ -1,14 +1,16 @@
 import math
 import numpy as np
 import time
+from solution import *
+from reader import *
 
 class Bound:
+
     def __init__(self, tree):
         self.tree = tree
         self.lower_bound = None
         self.timestamp_lower_bound = None
         self.upper_bound = None
-        self.timestamp_upper_bound = None
 
     def get_lower_bound_for_one_evac_node(self, id_evac_node):
         clock = 0
@@ -46,14 +48,18 @@ class Bound:
         time_limit = 1000
         data = self.tree
         time_list = []
+        evac_nodes_dict = {}
+        objective = 0
+
+        start_prog = time.time()
 
         # Compute the min time for each evac node
         for node_id in data.evac_node_id_list:
-            time = self.get_lower_bound_for_one_evac_node(node_id)
-            time_list.append((node_id, time))
+            time_one_node = self.get_lower_bound_for_one_evac_node(node_id)
+            time_list.append((node_id, time_one_node))
 
         # Order the list by time
-        time_list = sorted(time_list, key=lambda evac: evac[2], reverse=True)
+        time_list = sorted(time_list, key=lambda evac: evac[1], reverse=True)
 
         t_min = 0
         t_max = 0
@@ -74,7 +80,7 @@ class Bound:
 
             # for each arc of the evacuation road
             while father is not None:
-                index_arc = arc_list.index((father.father, father.son))
+                index_arc = arc_list.index((father.father.id1, father.son.id1))
                 interval += father.evac[node_id]
 
                 # look for the date when the not used capacity is enough
@@ -91,11 +97,17 @@ class Bound:
             interval = 0
             # for each arc of the evacuation road
             while father is not None:
-                index_arc = arc_list.index((father.father, father.son))
+                is_max = False
+                index_arc = arc_list.index((father.father.id1, father.son.id1))
                 interval += father.evac[node_id]
                 entire_gp_nb = node.population//node.max_rate
                 beg = t_min_current + interval
                 end = t_min_current+entire_gp_nb
+
+                if objective < (end + father.time):
+                    is_max = True
+                    objective = end + father.time
+
                 for t in range(beg, end):
                     gantt[index_arc][t] += node.max_rate
 
@@ -103,9 +115,25 @@ class Bound:
                 if rest != 0:
                     gantt[index_arc][end] += rest
                     end += 1
+                    if is_max:
+                        objective += 1
 
                 if t_max < end:
                     t_max = end
 
-            node.chosen_rate = node.max_rate
-            node.chosen_start_date = t_min_current
+            evac_nodes_dict[node_id] = {"evac_rate": node.max_rate, "start_date": t_min_current}
+
+        end_prog = time.time()
+        timestamp = end_prog - start_prog
+
+        self.upper_bound = Solution(data.filename, data, evac_nodes_dict, True, objective, timestamp, "Upper bound")
+
+
+# if __name__ == '__main__':
+#     read = Reader("TD.txt")
+#     bound = Bound(read.data)
+#     print("read data ok")
+#     bound.calculate_upper_bound()
+#     print("objective of the upper bound :")
+#     print(bound.upper_bound.objective)
+    #bound.upper_bound.write_solution()
