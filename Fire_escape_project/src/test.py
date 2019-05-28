809beeb2a20530f2d732c817e273683586b7205d
Vanhicia <vanhulle@etud.insa-toulouse.fr>
2019-05-28 15:35:32 +0200

    add test script

---
+++ Fire_escape_project/src/test.py
@@ -0,0 +1,17 @@
+import os
+from reader import *
+from bound import *
+
+
+if __name__ == '__main__':
+    instance_list = os.listdir("../instances")
+    instance_list.remove("TD.txt")
+
+    for instance in instance_list[2:]:
+        print(instance)
+        read = Reader(instance)
+
+        # Calculate the upper bound
+        bound = Bound(read.data)
+        bound.calculate_upper_bound()
+        bound.upper_bound.write_solution("solution_" + instance.rstrip(".full"))
